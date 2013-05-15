02c88feb6d0678ffa9e92cba2c03e134c7290129
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-05-15 09:52:41 -0400

    Changed handler signatures. Added triggers.

--- static/lib/wed/domlistener.js
+++ static/lib/wed/domlistener.js
@@ -15,6 +15,13 @@ MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
  * An "added-element" event is fired when an element is directly added
  * to the observed tree. The opposite event is "removed-element".
  *
+ * A "tigger" event with name [name] is fired when trigger([name]) is
+ * called. The event is fired as soon as all mutations being processed
+ * by the current invocation of _onMutation have been
+ * processed. Trigger events are meant to be tiggered by event
+ * handlers called by the Listener, not by outside code.
+ *
+ *
  * Example
  * 
  * Consider the following fragment:
@@ -35,25 +42,23 @@ MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
  * 
  * added-element, removed-element:
  *
- *  handler($root, $parent, $element)
+ *  handler($root, $parent, $previous_sibling, $next_sibling, $element)
  * 
- * There is no reason to provide $parent for an added-element event
- * but having the same signature for additions and removals allows to
- * use the same function for both cases. For removed-element events
- * the parent is the parent that the element had *before* it was
- * removed. The $element parameter is a single element.
+ * There is no reason to provide $parent, $previous_sibling,
+ * $next_sibling for an added-element event but having the same
+ * signature for additions and removals allows to use the same
+ * function for both cases. For removed-element events the parent is
+ * the parent that the element had *before* it was removed. The
+ * $element parameter is a single element.
  * 
  * included-element, excluded-element:
  *
- *  handler($root, $elements)
- * 
- * The $elements parameter is a list of elements.
+ *  handler($root, $element)
  * 
  * The order in which handlers are added matters. The Listener
- * provides the following guarantee:
- * 
- * - For any given (event, selector) pair, the handlers will be called
- *   in the order that they were added to the listener.
+ * provides the following guarantee: for any given type of event, the
+ * handlers will be called in the order that they were added to the
+ * listener.
  */
 
 function Listener(root) {
@@ -61,12 +66,16 @@ function Listener(root) {
     this._observer = new MutationObserver(this._onMutation.bind(this));
 
     this._event_handlers = {
-        "included-element": {},
-        "excluded-element": {},
-        "added-element": {},
-        "removed-element": {},
-        "children-changed": {}, // XXX do we want this?
+        "included-element": [],
+        "excluded-element": [],
+        "added-element": [],
+        "removed-element": [],
+        "children-changed": [], // XXX do we want this?
     };
+
+    this._trigger_handlers = {};
+
+    this._triggers_to_fire = {};
 }
 
 (function() {
@@ -88,19 +97,29 @@ function Listener(root) {
 
         for(var ev_ix = 0, event_type; event_type = event_types[ev_ix]; 
             ev_ix++) {
-            var map = this._event_handlers[event_type];
-            if (map === undefined)
-                throw new Error("invalid event_type: " + event_type);
-            
-            if (map[selector] === undefined)
-                map[selector] = [];
-            
-            map[selector].push(handler);
+            if (event_type !== "trigger") {
+                var pairs = this._event_handlers[event_type];
+                if (pairs === undefined)
+                    throw new Error("invalid event_type: " + event_type);
+                
+                pairs.push([selector, handler]);
+            }
+            else {
+                var handlers = this._trigger_handlers[selector];
+                if (handlers === undefined)
+                    handlers = this._trigger_handlers[selector] = [];
+
+                handlers.push(handler);
+            }
         }
     };
 
     this.stopListening = function () {
         this._observer.disconnect();
+    }
+
+    this.trigger = function(name) {
+        this._triggers_to_fire[name] = 1;
     }
 
     this._onMutation = function (mutations, observer) {
@@ -114,13 +133,13 @@ function Listener(root) {
                 //
                 // added-element
                 //
-                var sel_map = this._event_handlers["added-element"];
-                var selectors = Object.keys(sel_map);
+                var pairs = this._event_handlers["added-element"];
 
                 // Go over all the elements for which which we have handlers
-                for (var sel_ix = 0, sel_ix_limit = selectors.length;
-                     sel_ix < sel_ix_limit; ++sel_ix) {
-                    var sel = selectors[sel_ix];
+                for (var pair_ix = 0, pair_ix_limit = pairs.length;
+                     pair_ix < pair_ix_limit; ++pair_ix) {
+                    var pair = pairs[pair_ix];
+                    var sel = pair[0];
 
                     // Check whether any of the nodes contain an element we 
                     // care about.
@@ -128,8 +147,10 @@ function Listener(root) {
                          node_ix < node_ix_limit; ++node_ix) {
                         // These are the elements we are interested in.
                         if ($(mut.addedNodes[node_ix]).is(sel))
-                            this._callAllHandlers(sel_map[sel], 
+                            this._callAllHandlers(pair[1], 
                                                   $target,
+                                                  $(mut.previousSibling),
+                                                  $(mut.nextSibling),
                                                   $(mut.addedNodes[node_ix]));
                     }
                 }
@@ -137,20 +158,24 @@ function Listener(root) {
                 //
                 // removed-element
                 //
-                sel_map = this._event_handlers["removed-element"];
-                selectors = Object.keys(sel_map);
+                pairs = this._event_handlers["removed-element"];
 
                 // Go over all the elements for which which we have handlers
-                for (var sel_ix = 0, sel_ix_limit = selectors.length;
-                     sel_ix < sel_ix_limit; ++sel_ix) {
-                    var sel = selectors[sel_ix];
+                for (var pair_ix = 0, pair_ix_limit = pairs.length;
+                     pair_ix < pair_ix_limit; ++pair_ix) {
+                    var pair = pairs[pair_ix];
+                    var sel = pair[0];
 
                     // Check whether any of the nodes contain an element we 
                     // care about.
                     for (var node_ix = 0, node_ix_limit = mut.removedNodes.length;
                          node_ix < node_ix_limit; ++node_ix) {
                         if ($(mut.removedNodes[node_ix]).is(sel))
-                            this._callAllHandlers(sel_map[sel], $target, $(mut.removedNodes[node_ix]));
+                            this._callAllHandlers(pair[1], 
+                                                  $target, 
+                                                  $(mut.previousSibling),
+                                                  $(mut.nextSibling),
+                                                  $(mut.removedNodes[node_ix]));
                     }
                 }
 
@@ -158,37 +183,38 @@ function Listener(root) {
                 //
                 // included-element
                 //
-                sel_map = this._event_handlers["included-element"];
-                selectors = Object.keys(sel_map);
+                pairs = this._event_handlers["included-element"];
 
                 // Go over all the elements for which which we have handlers
-                for (var sel_ix = 0, sel_ix_limit = selectors.length;
-                     sel_ix < sel_ix_limit; ++sel_ix) {
-                    var sel = selectors[sel_ix];
+                for (var pair_ix = 0, pair_ix_limit = pairs.length;
+                     pair_ix < pair_ix_limit; ++pair_ix) {
+                    var pair = pairs[pair_ix];
+                    var sel = pair[0];
 
                     // Check whether any of the nodes contain an element we 
                     // care about.
                     for (var node_ix = 0, node_ix_limit = mut.addedNodes.length;
                          node_ix < node_ix_limit; ++node_ix) {
                         // These are the elements we are interested in.
-                        var $nodes = $(mut.addedNodes[node_ix]).
+                        var $targets = $(mut.addedNodes[node_ix]).
                             findAndSelf(sel);
 
-                        if ($nodes.length > 0)
-                            this._callAllHandlers(sel_map[sel], $nodes);
+                        for(var target_ix = 0, target_ix_limit = $targets.length; 
+                            target_ix < target_ix_limit; ++target_ix)
+                            this._callAllHandlers(pair[1], $($targets.get(target_ix)));
                     }
                 }
 
                 //
                 // excluded-element 
                 //
-                sel_map = this._event_handlers["excluded-element"];
-                selectors = Object.keys(sel_map);
+                pairs = this._event_handlers["excluded-element"];
 
                 // Go over all the elements for which which we have handlers
-                for (var sel_ix = 0, sel_ix_limit = selectors.length;
-                     sel_ix < sel_ix_limit; ++sel_ix) {
-                    var sel = selectors[sel_ix];
+                for (var pair_ix = 0, pair_ix_limit = pairs.length;
+                     pair_ix < pair_ix_limit; ++pair_ix) {
+                    var pair = pairs[pair_ix];
+                    var sel = pair[0];
 
                     // Check whether any of the nodes contain an element we 
                     // care about.
@@ -196,40 +222,66 @@ function Listener(root) {
                          mut.removedNodes.length;
                          node_ix < node_ix_limit; ++node_ix) {
                         // These are the elements we are interested in.
-                        var $nodes = $(mut.removedNodes[node_ix]).
+                        var $targets = $(mut.removedNodes[node_ix]).
                             findAndSelf(sel);
 
-                        if ($nodes.length > 0)
-                            this._callAllHandlers(sel_map[sel], $nodes);
+                        for(var target_ix = 0, target_ix_limit = $targets.length; 
+                            target_ix < target_ix_limit; ++target_ix)
+                            this._callAllHandlers(pair[1], $($targets.get(target_ix)));
                     }
                 }
 
                 // children-changed
-                sel_map = this._event_handlers["children-changed"];
-                selectors = Object.keys(sel_map);
+                pairs = this._event_handlers["children-changed"];
 
                 // Go over all the elements for which which we have handlers
-                for (var sel_ix = 0, sel_ix_limit = selectors.length;
-                     sel_ix < sel_ix_limit; ++sel_ix) {
-                    var sel = selectors[sel_ix];
+                for (var pair_ix = 0, pair_ix_limit = pairs.length;
+                     pair_ix < pair_ix_limit; ++pair_ix) {
+                    var pair = pairs[pair_ix];
+                    var sel = pair[0];
 
                     if ($target.is(sel))
-                        this._callAllHandlers(sel_map[sel], $target);
+                        this._callAllHandlers(pair[1], $target);
                 }
             }
+        }
+        
+        var keys = Object.keys(this._triggers_to_fire);
+        while(keys.length > 0) {
+            // We flush the map because the triggers could trigger
+            // more triggers. This also explains why we are in a loop.
+            this._triggers_to_fire = {};
+            
+            var trigger_map = this._trigger_handlers;
+            for (var key_ix = 0, key; (key = keys[key_ix]) !== undefined; 
+                 ++key_ix) {
+                var handlers = trigger_map[key];
+                if (handlers !== undefined) {
+                    for(var handler_ix = 0, handler; 
+                        (handler = handlers[handler_ix]) !== undefined;
+                        ++handler_ix)
+                        handler(this._$root);
+                }
+            }
+            
+            // See whether there is more to trigger.
+            keys = Object.keys(this._triggers_to_fire);
         }
     };
 
     this._callAllHandlers = function () {
-        var handlers = arguments[0];
+        var handler = arguments[0];
         var rest = Array.prototype.slice.call(arguments, 1);
         rest.unshift(this._$root);
-        for(var f_ix = 0, f_ix_limit = handlers.length;
-            f_ix < f_ix_limit; ++f_ix)
-            handlers[f_ix].apply(undefined, rest);
+        handler.apply(undefined, rest);
     };
 
 }).call(Listener.prototype);
+
+// Useful for debugging
+function _dumpMutation (mut) {
+    console.log(mut.type + ":", mut.target, mut.addedNodes, mut.removedNodes, mut.previousSibling, mut.nextSibling, mut.attributeName, mut.attributeNamespace, mut.oldValue);
+}
 
 exports.Listener = Listener;
 
