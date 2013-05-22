27b8dbed67e7be9d8f0b40aac3b8d6906769c49d
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-05-22 13:37:40 -0400

    Handle element creation in placeholders.

--- browser_test/parser_test.js
+++ browser_test/parser_test.js
@@ -1,5 +1,8 @@
-define(["module", "mocha/mocha", "chai", "jquery", "wed/parser", "wed/util", "salve/validate", "wed/domlistener", "wed/modes/generic/generic_decorator"], 
-function (module, mocha, chai, $, parser, util, validate, domlistener, decorator) {
+define(["module", "mocha/mocha", "chai", "jquery", "wed/parser", 
+        "wed/util", "salve/validate", "wed/domlistener", 
+        "wed/modes/generic/generic_decorator", "wed/transformation"], 
+function (module, mocha, chai, $, parser, util, validate, domlistener, 
+          generic_decorator, transformation) {
     var config = module.config();
     var schema = config.schema;
     var to_parse = config.to_parse;
@@ -169,131 +172,248 @@ function (module, mocha, chai, $, parser, util, validate, domlistener, decorator
         });
 
         describe("possibleAt", function () {
-            var p;
-            beforeEach(function () {
-                $data.empty();
-                p = new parser.Parser(schema, 
-                                      resolver,
-                                      $data.get(0));
-                p._max_timespan = 0; // Work forever.
-            });
-            
-            afterEach(function () {
-                $data.empty();
-                p = undefined;
-            });
-
-            function makeTest(name, stop_fn, no_load) {
-                it(name, function (done) {
-                    // Manipulate stop so that we know when the work is done.
-                    var old_stop = p.stop;
-                    p.stop = function () {
-                        stop_fn();
-                        done();
-                    };
-
-                    if (!no_load)
-                        require(["requirejs/text!" + to_parse], function(data) {
-                            $data.html(data);
-                            p.start();
-                        });
-                    else
-                        p.start();
+            describe("without decorations", function () {
+                var p;
+                beforeEach(function () {
+                    $data.empty();
+                    p = new parser.Parser(schema, 
+                                          resolver,
+                                          $data.get(0));
+                    p._max_timespan = 0; // Work forever.
                 });
-            }
+                
+                afterEach(function () {
+                    $data.empty();
+                    p = undefined;
+                });
 
-            makeTest("empty document, at root", function () {
-                var evs = p.possibleAt($data.get(0), 0);
-                assert.sameMembers(
-                    evs.toArray(), 
-                    [new validate.Event("enterStartTag", "", "html")]);
-            }, true);
+                function makeTest(name, stop_fn, no_load) {
+                    it(name, function (done) {
+                        // Manipulate stop so that we know when the work is done.
+                        var old_stop = p.stop;
+                        p.stop = function () {
+                            stop_fn();
+                            done();
+                        };
 
-            makeTest("with actual contents, at root", function () {
-                var evs = p.possibleAt($data.get(0), 0);
-                assert.sameMembers(
-                    evs.toArray(), 
-                    [new validate.Event("enterStartTag", "", "html")]);
-            });
+                        if (!no_load)
+                            require(["requirejs/text!" + to_parse], function(data) {
+                                $data.html(data);
+                                p.start();
+                            });
+                        else
+                            p.start();
+                    });
+                }
 
-            makeTest("with actual contents, at end", function () {
-                var evs = p.possibleAt($data.get(0), 1);
-                assert.sameMembers(
-                    evs.toArray(), 
-                    []);
-            });
-            
-            makeTest("with actual contents, start of html", function () {
-                var evs = p.possibleAt(
-                    $data.children("._real.html").get(0), 0);
-                assert.sameMembers(
-                    evs.toArray(), 
-                    [new validate.Event("enterStartTag", "", "head")]);
-            });
+                makeTest("empty document, at root", function () {
+                    var evs = p.possibleAt($data.get(0), 0);
+                    assert.sameMembers(
+                        evs.toArray(), 
+                        [new validate.Event("enterStartTag", "", "html")]);
+                }, true);
 
-            makeTest("with actual contents, start of head", function () {
-                var evs = p.possibleAt(
-                    $data.find("._real.head").get(0), 0);
-                assert.sameMembers(
-                    evs.toArray(), 
-                    [new validate.Event("enterStartTag", "", "title")]);
-            });
+                makeTest("with actual contents, at root", function () {
+                    var evs = p.possibleAt($data.get(0), 0);
+                    assert.sameMembers(
+                        evs.toArray(), 
+                        [new validate.Event("enterStartTag", "", "html")]);
+                });
 
-            makeTest(
-                "with actual contents, start of title (start of text node)", 
-                function () {
+                makeTest("with actual contents, at end", function () {
+                    var evs = p.possibleAt($data.get(0), 1);
+                    assert.sameMembers(
+                        evs.toArray(), 
+                        []);
+                });
+                
+                makeTest("with actual contents, start of html", function () {
                     var evs = p.possibleAt(
-                        $data.find("._real.title").get(0), 0);
+                        $data.children("._real.html").get(0), 0);
+                    assert.sameMembers(
+                        evs.toArray(), 
+                        [new validate.Event("enterStartTag", "", "head")]);
+                });
+
+                makeTest("with actual contents, start of head", function () {
+                    var evs = p.possibleAt(
+                        $data.find("._real.head").get(0), 0);
+                    assert.sameMembers(
+                        evs.toArray(), 
+                        [new validate.Event("enterStartTag", "", "title")]);
+                });
+
+                makeTest(
+                    "with actual contents, start of title (start of text node)", 
+                    function () {
+                        var el = $data.find("._real.title").get(0).childNodes[0];
+                        // Make sure we know what we are looking at.
+                        assert.equal(el.nodeType, Node.TEXT_NODE);
+                        var evs = p.possibleAt(el, 0);
+                        assert.sameMembers(
+                            evs.toArray(), 
+                            [new validate.Event("endTag", "", "title"),
+                             new validate.Event("text")]);
+                    });
+
+                makeTest(
+                    "with actual contents, index inside text node", 
+                    function () {
+                        var el = $data.find("._real.title").get(0).childNodes[0];
+                        // Make sure we know what we are looking at.
+                        assert.equal(el.nodeType, Node.TEXT_NODE);
+                        var evs = p.possibleAt(el, 1);
+                        assert.sameMembers(
+                            evs.toArray(), 
+                            [new validate.Event("endTag", "", "title"),
+                             new validate.Event("text")]);
+                    });
+
+                makeTest("with actual contents, end of title", function () {
+                    var title = $data.find("._real.title").get(0);
+                    var evs = p.possibleAt(title, title.childNodes.length);
                     assert.sameMembers(
                         evs.toArray(), 
                         [new validate.Event("endTag", "", "title"),
                          new validate.Event("text")]);
+
                 });
 
-            makeTest("with actual contents, index inside text node", 
-                     function () {
-                         var evs = p.possibleAt(
-                             $data.find("._real.title").get(0), 1);
-                         assert.sameMembers(
-                             evs.toArray(), 
-                             [new validate.Event("endTag", "", "title"),
-                              new validate.Event("text")]);
-                     });
+                makeTest("with actual contents, end of head", function () {
+                    var el = $data.find("._real.head").get(0);
+                    var evs = p.possibleAt(el, el.childNodes.length);
+                    assert.sameMembers(
+                        evs.toArray(), 
+                        [new validate.Event("endTag", "", "head")]);
 
+                });
 
-            makeTest("with actual contents, end of title", function () {
-                var title = $data.find("._real.title").get(0);
-                var evs = p.possibleAt(title, title.childNodes.length);
-                assert.sameMembers(
-                    evs.toArray(), 
-                    [new validate.Event("endTag", "", "title"),
-                     new validate.Event("text")]);
-
-            });
-
-            makeTest("with actual contents, end of head", function () {
-                var el = $data.find("._real.head").get(0);
-                var evs = p.possibleAt(el, el.childNodes.length);
-                assert.sameMembers(
-                    evs.toArray(), 
-                    [new validate.Event("endTag", "", "head")]);
+                makeTest("with actual contents, after head", function () {
+                    var el = $data.find("._real.head").get(0);
+                    var evs = p.possibleAt(
+                        el.parentNode, 
+                        Array.prototype.indexOf.call(
+                            el.parentNode.childNodes, el) + 1);
+                    assert.sameMembers(
+                        evs.toArray(), 
+                        [new validate.Event("enterStartTag", "", "body")]);
+                });
 
             });
 
-            makeTest("with actual contents, after head", function () {
-                var el = $data.find("._real.head").get(0);
-                var evs = p.possibleAt(
-                    el.parentNode, 
-                    Array.prototype.indexOf.call(
-                        el.parentNode.childNodes, el) + 1);
-                assert.sameMembers(
-                    evs.toArray(), 
-                    [new validate.Event("enterStartTag", "", "body")]);
+            describe("with decorations", function () {
+                var p;
+                beforeEach(function () {
+                    $data.empty();
+                    p = new parser.Parser(schema, 
+                                          resolver,
+                                          $data.get(0));
+                    p._max_timespan = 0; // Work forever.
+                });
+                
+                afterEach(function () {
+                    $data.empty();
+                    p = undefined;
+                });
+
+                function makeTest(name, stop_fn, transform_fn, no_load) {
+                    it(name, function (done) {
+                        // Manipulate stop so that we know when the work is done.
+                        var old_stop = p.stop;
+                        p.stop = function () {
+                            stop_fn();
+                            done();
+                        };
+
+                        if (!no_load)
+                            require(["requirejs/text!" + to_parse], function(data) {
+                                $data.html(data);
+                                var listener = 
+                                    new domlistener.Listener($data.get(0));
+                                var decorator = 
+                                    new generic_decorator.GenericDecorator(
+                                        listener);
+                                decorator.init($data);
+                                if (transform_fn) 
+                                    transform_fn();
+                                p.start();
+                            });
+                        else
+                            p.start();
+                    });
+                }
+                
+                makeTest("with actual contents, start of html, " + 
+                         "before decoration element", 
+                         function () {
+                             var el = $data.children("._real.html").get(0);
+                             var evs = p.possibleAt(el, 0);
+                             // Make sure we are looking at a gui element
+                             assert.isTrue($(el.childNodes[0]).is("._gui"));
+                             assert.sameMembers(
+                                 evs.toArray(), 
+                                 [new validate.Event("enterStartTag", "", 
+                                                     "head")]);
+                });
+                makeTest("with actual contents, start of html, " + 
+                         "after decoration element", 
+                         function () {
+                             var el = $data.children("._real.html").get(0);
+                             var evs = p.possibleAt(el, 1);
+                             // Make sure we are looking at a real element
+                             assert.isTrue($(el.childNodes[1]).is("._real"));
+                             assert.sameMembers(
+                                 evs.toArray(), 
+                                 [new validate.Event("enterStartTag", "", "head")]);
+                });
+                makeTest(
+                    "with actual contents, start of title (start of text node)", 
+                    function () {
+                        var el = $data.find("._real.title").get(0).childNodes[1];
+                        // Make sure we know what we are looking at.
+                        assert.equal(el.nodeType, Node.TEXT_NODE);
+                        var evs = p.possibleAt(el, 0);
+                        assert.sameMembers(
+                            evs.toArray(), 
+                            [new validate.Event("endTag", "", "title"),
+                             new validate.Event("text")]);
+                    });
+
+                makeTest(
+                    "with actual contents, index inside text node", 
+                    function () {
+                        var el = $data.find("._real.title").get(0).childNodes[1];
+                        // Make sure we know what we are looking at.
+                        assert.equal(el.nodeType, Node.TEXT_NODE);
+                        var evs = p.possibleAt(el, 1);
+                        assert.sameMembers(
+                            evs.toArray(), 
+                            [new validate.Event("endTag", "", "title"),
+                             new validate.Event("text")]);
+                    });
+
+                makeTest("in placeholder",
+                         function () {
+                             var el = $data.find("._real.em").get(0).childNodes[1];
+                             // Make sure we know what we are looking at.
+                             assert.isTrue($(el).is("._placeholder"));
+                             var evs = p.possibleAt(el, 0);
+                             console.log(evs.toArray());
+                             assert.sameMembers(
+                                 evs.toArray(), 
+                                 [new validate.Event("endTag", "", "em"),
+                                  new validate.Event("enterStartTag", "", "em"),
+                                  new validate.Event("text")]);
+                         },
+                         // This modifies the tree
+                         function () {
+                             var $el = $data.find("._real.body");
+                             transformation.insertElement(null, $el.get(0), 0, "em");
+                         });
             });
-
-
             
         });
+        
 
     });
 });
