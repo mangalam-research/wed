6c425751b4244515750b472f82691860f869d6da
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-05-29 14:56:42 -0400

    Added tests for xml-to-html.xsl

---
+++ test/lib/wed/xml-to-html_test.js
@@ -0,0 +1,32 @@
+'use strict';
+var child_process = require("child_process");
+var fs = require("fs");
+var assert = require("chai").assert;
+// Reminder: paths are relative to where mocha is run.
+// I.e. the root of our source tree!
+var XSL = "lib/wed/xml-to-html.xsl";
+describe("xml-to-html", function () {
+    function makeTest(name) {
+        var converted_name = name.replace(/ /g, '-');
+        it(name, function (done) {
+            var source = "test/lib/wed/" + converted_name + ".xml";
+            var p = child_process.spawn('saxon', ['-xsl:' + XSL, 
+                                                  '-s:' + source]);
+            var html = [];
+            var data = p.stdout.on('data', html.push.bind(html, data));
+            p.on('close', function (code) {
+                assert.equal(code, 0, "saxon failed");
+                assert.equal(html.join(""), 
+                             fs.readFileSync('test/lib/wed/' + 
+                                             converted_name + ".html", 
+                                             'utf-8'));
+                done();
+            });
+        });
+    }
+    
+    makeTest("should convert xml to html");
+    makeTest("should encode name prefixes");
+    makeTest("should encode dashes in attribute names");
+    makeTest("should encode namespace changes");
+});
