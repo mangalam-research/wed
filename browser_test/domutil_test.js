fc97bdbfa0747338fd0c91de43f7cb6c42ca5d51
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-06-06 16:25:26 -0400

    nodeAtPrevCaretPosition is now prevCaretPosition

--- browser_test/domutil_test.js
+++ browser_test/domutil_test.js
@@ -141,7 +141,7 @@ function (mocha, chai, $, domutil) {
             });
         });
 
-        describe("nodeAtPrevCaretPosition", function () {
+        describe("prevCaretPosition", function () {
             var $root = $("#domroot");
             var caret;
             before(function () {
@@ -152,24 +152,33 @@ function (mocha, chai, $, domutil) {
                 $root.empty();
             });
 
-            function testPair(node_of_interest, text_node_of_interest) {
-                if (text_node_of_interest === undefined)
-                    text_node_of_interest = node_of_interest.childNodes[node_of_interest.childNodes.length - 1];
+            function testPair(no_text_expected,
+                              text_expected) {
+                if (text_expected === undefined)
+                    text_expected = no_text_expected;
 
                 // The isNotNull checks are to ensure we don't majorly
                 // screw up in setting up a test case. For instance if
                 // we have $data = $(""), the assert.equal test would
                 // likely compare null to null, and would pass.
                 it("no_text === true", function () {
-                    assert.isNotNull(node_of_interest);
+                    assert.isNotNull(no_text_expected[0]);
 
-                    assert.equal(domutil.nodeAtPrevCaretPosition(caret),
-                                 node_of_interest);
+                    var result = domutil.prevCaretPosition(caret);
+                    assert.equal(result[0], no_text_expected[0],
+                                 "node");
+                    assert.equal(result[1], no_text_expected[1],
+                                 "offset");
                 });
                 it("no_text === false", function () {
-                    assert.isNotNull(node_of_interest);
-                    assert.equal(domutil.nodeAtPrevCaretPosition(caret, false),
-                                 text_node_of_interest);
+                    assert.isNotNull(text_expected[0]);
+
+                    var result = domutil.prevCaretPosition(caret,
+                                                           false);
+                    assert.equal(result[0], text_expected[0],
+                                 "node");
+                    assert.equal(result[1], text_expected[1],
+                                "offset");
                 });
             }
 
@@ -180,7 +189,8 @@ function (mocha, chai, $, domutil) {
                     $root.append($data);
                     caret = [$data.get(0).childNodes[0], 2];
                 });
-                testPair($data.get(0));
+                testPair([$data.get(0), 0],
+                         [$data.get(0).childNodes[0], 1]);
             });
             describe("move into child", function () {
                 var $data = $("<span><b>test</b> test</span>");
@@ -188,60 +198,80 @@ function (mocha, chai, $, domutil) {
                     $root.empty();
                     $root.append($data);
                     var node = $data.get(0);
-                    caret = [node.childNodes[node.childNodes.length - 1], 0];
+                    // This puts the caret at the start of the
+                    // last text node.
+                    caret = [node.childNodes[
+                        node.childNodes.length - 1], 0];
                 });
-                testPair($data.children("b").get(0));
+                testPair([$data.children("b").get(0), 0],
+                         [$data.children("b").get(0).childNodes[0], 4]);
             });
             describe("move to parent", function () {
                 var $data = $("<span>test <b>test</b></span>");
                 beforeEach(function () {
                     $root.empty();
                     $root.append($data);
-                    caret = [$data.children("b").get(0).childNodes[0], 0];
+                    // This puts the caret at the start of the text
+                    // node in <b>
+                    caret = [$data.children("b").get(0).childNodes[0],
+                             0];
                 });
-                testPair($data.get(0), $data.get(0).childNodes[0]);
+                testPair([$data.get(0), 1]);
             });
-            describe("skip empty elements", function () {
+            describe("enter empty elements", function () {
                 var $data = $("<span><i>a</i><i></i><i>b</i></span>");
                 beforeEach(function () {
                     $root.empty();
                     $root.append($data);
-                    caret = [$data.children("i").get(2).childNodes[0], 0];
+                    // This puts the caret after the 2nd <i>.
+                    caret = [$data.get(0), 2];
                 });
-                testPair($data.children("i").get(0));
+                testPair([$data.children("i").get(1), 0]);
             });
             describe("white-space: normal", function () {
-                // The case is designed so that it skips over the white space
-                var $data = $("<span><s>test</s><s>   test</s></span>");
+                // The case is designed so that it skips over the
+                // white space
+                var $data =
+                    $("<span><s>test</s><s>   test</s></span>");
                 beforeEach(function () {
                     $root.empty();
                     $root.append($data);
-                    caret = [$data.children("s").get(1).childNodes[0], 3];
+                    // Place the caret just after the white space
+                    // in the 2nd <s> node.
+                    caret = [$data.children("s").get(1).childNodes[0],
+                             3];
                 });
-                testPair($data.children("s").get(0));
+                testPair([$data.get(0), 1]);
             });
             describe("white-space: pre", function () {
                 // The case is designed so that it does not skip over
                 // the white space.
-                var $data = $("<span><s>test</s>" +
-                              "<s style='white-space: pre'>   test</s></span>");
+                var $data =
+                    $("<span><s>test</s>" +
+                      "<s style='white-space: pre'>   test</s>"+
+                      "</span>");
                 beforeEach(function () {
                     $root.empty();
                     $root.append($data);
-                    caret = [$data.children("s").get(1).childNodes[0], 3];
+                    // Place the caret just after the white space
+                    // in the 2nd <s> node.
+                    caret = [$data.children("s").get(1).childNodes[0],
+                             3];
                 });
-                testPair($data.children("s").get(1));
+                testPair([$data.children("s").get(1), 0],
+                         [$data.children("s").get(1).childNodes[0],
+                          2]);
             });
             describe("can't find a node", function () {
                 beforeEach(function () {
                     caret = [$("html").get(0), 0];
                 });
                 it("no_text === true", function () {
-                    assert.isNull(domutil.nodeAtPrevCaretPosition(caret));
+                    assert.isNull(domutil.prevCaretPosition(caret));
                 });
                 it("no_text === false", function () {
-                    assert.isNull(domutil.nodeAtPrevCaretPosition(caret,
-                                                                  false));
+                    assert.isNull(domutil.prevCaretPosition(caret,
+                                                            false));
                 });
             });
         });
