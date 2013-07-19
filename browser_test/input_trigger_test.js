040861079ff35e02afe4dfe519fdf661b99c670c
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-07-19 09:40:08 -0400

    Added input_trigger and its tests.

---
+++ browser_test/input_trigger_test.js
@@ -0,0 +1,162 @@
+define(["mocha/mocha", "chai", "jquery", "wed/domlistener",
+        "wed/input_trigger", "wed/wed", "wed/key"],
+function (mocha, chai, $, domlistener, input_trigger, wed, key) {
+var assert = chai.assert;
+var Listener = domlistener.Listener;
+var InputTrigger = input_trigger.InputTrigger;
+
+var options = {
+    schema: 'test/tei-simplified-rng.js',
+    mode: {
+        path: 'wed/modes/generic/generic',
+        options: {
+            meta: 'wed/modes/generic/metas/tei_meta'
+        }
+    }
+};
+
+var wedroot = $("#wedframe-invisible").contents().find("#wedroot").get(0);
+var $wedroot = $(wedroot);
+var source = "../../test-files/input_trigger_test_data/source_converted.xml";
+
+describe("InputTrigger", function () {
+    var editor;
+    beforeEach(function (done) {
+        $wedroot.empty();
+        require(["requirejs/text!" + source], function(data) {
+            $wedroot.append(data);
+            editor = new wed.Editor();
+            editor.addEventListener("initialized", function () {
+                done();
+            });
+            editor.init(wedroot, options);
+        });
+    });
+
+    afterEach(function () {
+        if (editor)
+            editor.destroy();
+        editor = undefined;
+    });
+
+    it("triggers on children-changed events", function () {
+        var input_trigger = new InputTrigger(editor, ".p");
+        var seen = 0;
+        var $p = editor.$tree_root.find(".p").last();
+        input_trigger.addKeyHandler(key.makeKey(";"), function (type, $el) {
+            assert.equal(type, "children-changed");
+            assert.equal($el.get(0), $p.get(0));
+            seen++;
+        });
+        var text = document.createTextNode("abc;def");
+        $p.append(text);
+        editor._syncDisplay();
+        assert.equal(seen, 1);
+    });
+
+    it("triggers on text-changed events", function () {
+        var input_trigger = new InputTrigger(editor, ".p");
+        var seen = 0;
+        var $p = editor.$tree_root.find(".p").last();
+        input_trigger.addKeyHandler(key.makeKey(";"), function (type, $el) {
+            assert.equal(type, "text-changed");
+            assert.equal($el.get(0), $p.get(0));
+            seen++;
+        });
+
+        var text = $p.get(0).lastChild;
+        // Make sure we're looking at the right thing.
+        assert.equal(text.nodeValue, " blah.");
+
+        // Initiate the change.
+        text.nodeValue = " bl;a";
+        editor._syncDisplay();
+        assert.equal(seen, 1);
+    });
+
+    it("triggers on input events", function () {
+        var input_trigger = new InputTrigger(editor, ".p");
+        var seen = 0;
+        input_trigger.addKeyHandler(key.makeKey(";"), function (type, $el) {
+            assert.equal(type, "keydown");
+            assert.equal($el.get(0),
+                         editor.$tree_root.find(".p").last().get(0));
+            seen++;
+        });
+
+        // Synthetic event
+        var event = new $.Event("keydown");
+        var my_key = key.makeKey(";");
+        event.which = my_key.which;
+        event.keyCode = my_key.keyCode;
+        event.charCode = my_key.charCode;
+        event.ctrlKey = my_key.ctrlKey;
+        event.altKey = my_key.altKey;
+        event.metaKey = my_key.metaKey;
+        editor.setCaret(editor.$root.find(".p").last().get(0), 0);
+        editor.$root.trigger(event);
+        assert.equal(seen, 1);
+    });
+
+    it("does not trigger on unimportant children-changed events", function () {
+        var input_trigger = new InputTrigger(editor, ".p");
+        var seen = 0;
+        var $p = editor.$tree_root.find(".p").last();
+        input_trigger.addKeyHandler(key.makeKey(";"), function (type, $el) {
+            assert.equal(type, "children-changed");
+            assert.equal($el.get(0), $p.get(0));
+            seen++;
+        });
+        var text = document.createTextNode("abcdef");
+        $p.append(text);
+        editor._syncDisplay();
+        assert.equal(seen, 0);
+    });
+
+    it("does not trigger on unimportant text-changed events", function () {
+        var input_trigger = new InputTrigger(editor, ".p");
+        var seen = 0;
+        var $p = editor.$tree_root.find(".p").last();
+        input_trigger.addKeyHandler(key.makeKey(";"), function (type, $el) {
+            assert.equal(type, "text-changed");
+            assert.equal($el.get(0), $p.get(0));
+            seen++;
+        });
+
+        var text = $p.get(0).lastChild;
+        // Make sure we're looking at the right thing.
+        assert.equal(text.nodeValue, " blah.");
+
+        // Initiate the change.
+        text.nodeValue = " blah...";
+        editor._syncDisplay();
+        assert.equal(seen, 0);
+    });
+
+    it("does not trigger on unimportant input events", function () {
+        var input_trigger = new InputTrigger(editor, ".p");
+        var seen = 0;
+        input_trigger.addKeyHandler(key.makeKey(";"), function (type, $el) {
+            assert.equal(type, "keydown");
+            assert.equal($el.get(0),
+                         editor.$tree_root.find(".p").last().get(0));
+            seen++;
+        });
+
+        // Synthetic event
+        var event = new $.Event("keydown");
+        var my_key = key.makeKey(":");
+        event.which = my_key.which;
+        event.keyCode = my_key.keyCode;
+        event.charCode = my_key.charCode;
+        event.ctrlKey = my_key.ctrlKey;
+        event.altKey = my_key.altKey;
+        event.metaKey = my_key.metaKey;
+        editor.setCaret(editor.$root.find(".p").last().get(0), 0);
+        editor.$root.trigger(event);
+        assert.equal(seen, 0);
+    });
+
+});
+
+});
