558211b7b4b67df167391762b85cea4ac29c3cb2
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-10-01 19:55:52 -0400

    Moved to selenic.

--- config/selenium_test_config.py
+++ config/selenium_test_config.py
@@ -1,47 +1,9 @@
-import httplib
-import base64
-try:
-    import json
-except ImportError:
-    import simplejson as json
-import os
+import selenic
 
-from selenium import webdriver
+# If you need something more complex than what
+# selenium_local_config.py allows, you'll have to copy this to your
+# local_config directory and edit it.
 
 
-_dirname = os.path.dirname(__file__)
-
-local_conf = {}
-execfile(os.path.join(_dirname, "selenium_local_config.py"), local_conf)
-
-
-def get_driver():
-    if os.environ.get("SELENIUM_SAUCELABS"):
-        desired_capabilities = webdriver.DesiredCapabilities.CHROME
-        desired_capabilities['platform'] = 'Linux'
-        desired_capabilities['name'] = 'Wed test'
-
-        driver = webdriver.Remote(
-            desired_capabilities=desired_capabilities,
-            command_executor="http://" +
-            local_conf["SAUCELABS_CREDENTIALS"] +
-            "@ondemand.saucelabs.com:80/wd/hub"
-        )
-    else:
-        driver = webdriver.Chrome(local_conf["CHROMEDRIVER_PATH"])
-
-    return driver
-
-
-def set_test_status(jobid, passed=True):
-    if os.environ.get("SELENIUM_SAUCELABS"):
-        (username, key) = local_conf["SAUCELABS_CREDENTIALS"].split(":")
-        creds = base64.encodestring('%s:%s' % (username, key))[:-1]
-
-        conn = httplib.HTTPConnection("saucelabs.com")
-        conn.request('PUT', '/rest/v1/%s/jobs/%s' % (username, jobid),
-                     json.dumps({"passed": passed}),
-                     headers={"Authorization": "Basic " + creds})
-        return conn.getresponse() == 200
-    else:
-        return True
+class Config(selenic.Config):
+    pass
