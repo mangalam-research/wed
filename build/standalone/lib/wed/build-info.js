19914d6a9350d3af23a631c99b924cdbc9164e02
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-10-28 16:40:24 -0400

    New release.

--- build/standalone/lib/wed/build-info.js
+++ build/standalone/lib/wed/build-info.js
@@ -1,7 +1,7 @@
 define([], function () {
 return {
-    desc: 'v0.10.0-x-3-gf6d0313',
-    date: 'Mon Oct 28 2013 14:53:21 GMT-0400 (EDT)'
+    desc: 'v0.10.0-x-5-ga2525d9-unclean',
+    date: 'Mon Oct 28 2013 16:37:30 GMT-0400 (EDT)'
 };
 });
 
