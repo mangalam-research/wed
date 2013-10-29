dcbbec7884cc805b2616e68b3565ce4500bdc40a
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-10-29 09:36:26 -0400

    Added a sample document.

--- build/standalone/lib/wed/build-info.js
+++ build/standalone/lib/wed/build-info.js
@@ -1,7 +1,7 @@
 define([], function () {
 return {
-    desc: 'v0.10.0-x-7-g070157c-unclean',
-    date: 'Tue Oct 29 2013 07:57:44 GMT-0400 (EDT)'
+    desc: 'v0.10.0-x-8-geb24477-unclean',
+    date: 'Tue Oct 29 2013 09:34:05 GMT-0400 (EDT)'
 };
 });
 
