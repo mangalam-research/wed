34d33c84bf8ea91bb0b34d39c5f4b5614562c93e
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-11-13 16:35:01 -0500

    Fixed the error with the clean target. Removed an errant $(info...).

--- build.mk
+++ build.mk
@@ -133,8 +133,10 @@ build-config: $(CONFIG_TARGETS) | build/config
 build/config: | build-dir
 	mkdir $@
 
+ifneq ($(filter clean,$(MAKECMDGOALS)),clean)
 # See Makefile for the flip side of this.
 include $(CONFIG_DEPS)
+endif
 
 # Here are the actual targets that build the actual config files.
 build/config/%:
@@ -347,7 +349,6 @@ selenium-test: build | build-test-files
 doc: rst-doc jsdoc3-doc
 
 JSDOC3_TEMPLATE_TARGETS=$(patsubst $(JSDOC3_DEFAULT_TEMPLATE)/%,build/jsdoc_template/%,$(shell find $(JSDOC3_DEFAULT_TEMPLATE) -type f))
-$(info $(JSDOC3_TEMPLATE_TARGETS))
 
 .PHONY: jsdoc3-doc
 jsdoc3-doc: $(JSDOC3_TEMPLATE_TARGETS) build/jsdoc_template/static/styles/wed.css
@@ -377,7 +378,7 @@ rst-doc: $(HTML_TARGETS)
 clean::
 	-rm -rf build
 	-rm $(HTML_TARGETS)
-	-rm gh-pages-build
+	-rm -rf gh-pages-build
 
 .PHONY: distclean
 distclean: clean
