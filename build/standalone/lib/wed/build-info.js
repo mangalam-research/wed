15242d362a3e4d19b54201de1c0f4b72d33d40a5
Louis-Dominique Dubeau <ldd@lddubeau.com>
2014-12-06 07:42:51 -0500

    Version 0.20.2

--- build/standalone/lib/wed/build-info.js
+++ build/standalone/lib/wed/build-info.js
@@ -1,7 +1,7 @@
 define([], function () {
 return {
-    desc: 'v0.20.1',
-    date: 'Wed Nov 26 2014 15:51:31 GMT-0500 (EST)'
+    desc: 'v0.20.2',
+    date: 'Sat Dec 06 2014 07:41:19 GMT-0500 (EST)'
 };
 });
 
