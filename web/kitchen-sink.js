94329fbce2fc4544a4d1ff758aead82b5ed3f466
Louis-Dominique Dubeau <ldd@lddubeau.com>
2017-08-10 13:38:36 -0400

    Add a way to load options through a query.

--- web/kitchen-sink.js
+++ web/kitchen-sink.js
@@ -5,6 +5,7 @@
  * @license MPL 2.0
  * @copyright Mangalam Research Center for Buddhist Languages
  */
+/* global Promise */
 define(function f(require) {
   "use strict";
 
@@ -16,7 +17,6 @@ define(function f(require) {
   var onerror = require("wed/onerror");
   var globalConfig = require("global-config");
   var mergeOptions = require("merge-options");
-  var Promise = require("bluebird");
 
   // This installs last-resort on our current window and registers with it
   // wed's error handler.
@@ -29,80 +29,91 @@ define(function f(require) {
   var file = query.file;
   var schema = query.schema;
   var options_param = query.options;
+  var fetchOptions = query.fetchOptions;
 
-  var options = {};
-  if (mode) {
-    options.mode = { path: mode };
-  }
-
-  if (schema) {
-    switch (schema) {
-    case "@math":
-      options.schema = "../schemas/tei-math-rng.js";
-      options.mode = {
-        path: "wed/modes/generic/generic",
-        options: {
-          metadata: "../schemas/tei-math-metadata.json",
-        },
-      };
-      break;
-    case "@docbook":
-      options.schema = "../schemas/docbook.js";
-      options.mode = {
-        path: "wed/modes/generic/generic",
-        options: {
-          metadata: "../schemas/docbook-metadata.json",
-        },
-      };
-      break;
-    default:
-      options.schema = schema;
+  function fetched(options) {
+    if (mode) {
+      options.mode = { path: mode };
     }
-  }
 
-  if (options_param === "noautoinsert") {
-    options.mode.options = { autoinsert: false };
-  }
-
-  if (options_param === "ambiguous_fileDesc_insert") {
-    options.mode.options = { ambiguous_fileDesc_insert: true };
-  }
-
-  if (options_param === "fileDesc_insert_needs_input") {
-    options.mode.options = { fileDesc_insert_needs_input: true };
-  }
-
-  if (options_param === "hide_attributes") {
-    options.mode.options = { hide_attributes: true };
-  }
-
-  var r = new runtime.Runtime(options);
-
-  var deps = [];
-  if (file) {
-    deps.push(file);
-  }
-
-  var text;
-  Promise.all(deps.map(r.resolve.bind(r)))
-    .then(function resolved(resolvedDeps) {
-      if (deps.length === 0) {
-        return;
+    if (schema) {
+      switch (schema) {
+      case "@math":
+        options.schema = "../schemas/tei-math-rng.js";
+        options.mode = {
+          path: "wed/modes/generic/generic",
+          options: {
+            metadata: "../schemas/tei-math-metadata.json",
+          },
+        };
+        break;
+      case "@docbook":
+        options.schema = "../schemas/docbook.js";
+        options.mode = {
+          path: "wed/modes/generic/generic",
+          options: {
+            metadata: "../schemas/docbook-metadata.json",
+          },
+        };
+        break;
+      default:
+        options.schema = schema;
       }
+    }
 
-      var resolvedFile = resolvedDeps[0];
-      if (!resolvedFile) {
-        throw new Error("did not resolve file! " + deps[0]);
-      }
+    if (options_param === "noautoinsert") {
+      options.mode.options = { autoinsert: false };
+    }
 
-      text = resolvedFile;
-    })
-    .then(function start() {
-      $(function ready() {
-        var widget = document.getElementById("widget");
-        var finalOptions = mergeOptions({}, globalConfig.config, options);
-        window.wed_editor = new wed.Editor(widget, finalOptions);
-        window.wed_editor.init(text);
+    if (options_param === "ambiguous_fileDesc_insert") {
+      options.mode.options = { ambiguous_fileDesc_insert: true };
+    }
+
+    if (options_param === "fileDesc_insert_needs_input") {
+      options.mode.options = { fileDesc_insert_needs_input: true };
+    }
+
+    if (options_param === "hide_attributes") {
+      options.mode.options = { hide_attributes: true };
+    }
+
+    var r = new runtime.Runtime(options);
+
+    var deps = [];
+    if (file) {
+      deps.push(file);
+    }
+
+    var text;
+    Promise.all(deps.map(r.resolve.bind(r)))
+      .then(function resolved(resolvedDeps) {
+        if (deps.length === 0) {
+          return;
+        }
+
+        var resolvedFile = resolvedDeps[0];
+        if (!resolvedFile) {
+          throw new Error("did not resolve file! " + deps[0]);
+        }
+
+        text = resolvedFile;
+      })
+      .then(function start() {
+        $(function ready() {
+          var widget = document.getElementById("widget");
+          var finalOptions = mergeOptions({}, globalConfig.config, options);
+          window.wed_editor = new wed.Editor(widget, finalOptions);
+          window.wed_editor.init(text);
+        });
       });
+  }
+
+  if (fetchOptions) {
+    require([fetchOptions], function loaded(module) {
+      fetched(module.config);
     });
+  }
+  else {
+    fetched({});
+  }
 });
