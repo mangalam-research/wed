539b7d60dc53f04335793102cd87c0cca141b7b6
Louis-Dominique Dubeau <ldd@lddubeau.com>
2017-09-27 16:31:29 -0400

    Move attribute hiding tests to Karma.

---
+++ lib/tests/integration/wed-hide-attributes-test.ts
@@ -0,0 +1,101 @@
+/**
+ * @author Louis-Dominique Dubeau
+ * @license MPL 2.0
+ * @copyright Mangalam Research Center for Buddhist Languages
+ */
+import { expect } from "chai";
+import * as mergeOptions from "merge-options";
+import * as sinon from "sinon";
+
+import * as log from "wed/log";
+import * as onerror from "wed/onerror";
+import * as wed from "wed/wed";
+
+import * as globalConfig from "../base-config";
+import { DataProvider, makeWedRoot, setupServer } from "../util";
+
+const options = {
+  schema: "/base/build/schemas/tei-simplified-rng.js",
+  mode: {
+    path: "wed/modes/test/test-mode",
+    options: {
+      metadata: "/base/build/schemas/tei-metadata.json",
+      hide_attributes: true,
+    },
+  },
+};
+
+const assert = chai.assert;
+
+describe("wed hide attributes:", () => {
+  let source: string;
+  let editor: wed.Editor;
+  let topSandbox: sinon.SinonSandbox;
+  let wedroot: HTMLElement;
+
+  before(async () => {
+    const provider =
+      new DataProvider("/base/build/standalone/lib/tests/wed_test_data/");
+    source = await provider.getText("source_converted.xml");
+  });
+
+  before(() => {
+    topSandbox = sinon.sandbox.create({
+      useFakeServer: true,
+    });
+    setupServer(topSandbox.server);
+
+    wedroot = makeWedRoot(document);
+    document.body.appendChild(wedroot);
+    editor = new wed.Editor(wedroot,
+                            mergeOptions(globalConfig.config, options));
+    return editor.init(source)
+      .then(() => {
+        // tslint:disable-next-line:no-any
+        (editor.validator as any)._validateUpTo(editor.dataRoot, -1);
+      });
+  });
+
+  beforeEach(() => {
+    editor.undoAll();
+    editor.resetLabelVisibilityLevel();
+  });
+
+  afterEach(() => {
+    assert.isFalse(onerror.is_terminating(),
+                   "test caused an unhandled exception to occur");
+    // We don't reload our page so we need to do this.
+    onerror.__test.reset();
+    editor.editingMenuManager.dismiss();
+  });
+
+  after(() => {
+    if (editor !== undefined) {
+      editor.destroy();
+    }
+
+    // We read the state, reset, and do the assertion later so that if the
+    // assertion fails, we still have our reset.
+    const wasTerminating = onerror.is_terminating();
+
+    // We don't reload our page so we need to do this.
+    onerror.__test.reset();
+    log.clearAppenders();
+    expect(wasTerminating)
+      .to.equal(false, "test caused an unhandled exception to occur");
+
+    // tslint:disable-next-line:no-any
+    (editor as any) = undefined;
+
+    if (topSandbox !== undefined) {
+      topSandbox.restore();
+    }
+    document.body.removeChild(wedroot);
+  });
+
+  // We don't put this with wed-validation-error-test because it is not
+  // specifically checking the validation code but is an overall smoketest.
+  it("is able to start", () => {
+    return editor.firstValidationComplete;
+  });
+});
